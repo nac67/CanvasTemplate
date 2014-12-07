fd6eeff4bb0c42d9d3f7e1ab38af913da374da99
nac67 <nac67@cornell.edu>
2014-12-07 16:11:14 -0500

    cleaned

--- src/contentmanager/filmstrip.js
+++ src/contentmanager/filmstrip.js
@@ -4,15 +4,15 @@ var Filmstrip = function (bmp, frW, frH, numF, frR) {
     this.frameWidth = frW;       // the width of an individual frame
     this.frameHeight = frH;      // the height of an individual frame
     this.numFramesX = frR;       // the number of frames per row in the the filmstrip
+}
 
-    /** draws the image's top left corner at x,y */
-    this.draw = function(frame, x, y, alpha){
-        if(typeof alpha === "undefined") alpha = 1;
-        context.globalAlpha = alpha;
-        frame = frame%this.numFrames;
-        var drawX = frame%this.numFramesX * this.frameWidth;
-        var drawY = Math.floor(frame/this.numFramesX) * this.frameHeight;
-        context.drawImage(this.srcImage, drawX, drawY, this.frameWidth, this.frameHeight,x,y,this.frameWidth,this.frameHeight);
-        context.globalAlpha = 1;
-    }
+/** draws the image's top left corner at x,y */
+Filmstrip.prototype.draw = function(frame, x, y, alpha){
+    if(typeof alpha === "undefined") alpha = 1;
+    context.globalAlpha = alpha;
+    frame = frame%this.numFrames;
+    var drawX = frame%this.numFramesX * this.frameWidth;
+    var drawY = Math.floor(frame/this.numFramesX) * this.frameHeight;
+    context.drawImage(this.srcImage, drawX, drawY, this.frameWidth, this.frameHeight,x,y,this.frameWidth,this.frameHeight);
+    context.globalAlpha = 1;
 }
