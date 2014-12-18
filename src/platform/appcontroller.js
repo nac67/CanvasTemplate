ae3344361e6a64171a636f5a4eb26d1aa709fec5
nac67 <nac67@cornell.edu>
2014-12-18 15:58:05 -0500

    corners work

--- src/platform/appcontroller.js
+++ src/platform/appcontroller.js
@@ -13,11 +13,44 @@ var AppController = function () {
     this.player = new Character();
 }
 
+//checks if (x,y) is inside a world cell,
+// includeTop determines if the y position is sitting
+// exactly where the top of a cell is, whether to include
+// that in the collision. This is important if you want
+// to make something move left and right, you don't want
+// to include the top because then the player will think his
+// feet are hitting a wall, when they're just touching the ground.
+// likewise, we need to register a collision for determining if 
+// the player is grounded.
+AppController.prototype.inCell = function(x, y, includeTop) {
+    if (includeTop === undefined) { 
+        includeTop = true;
+    }
+    var inTileX = x % 1;
+    var inTileY = y % 1;
+    var tileX = Math.floor(x);
+    var tileY = Math.floor(y);
+    try {
+        var occupied = this.map[tileY][tileX] === 1;
+        if (includeTop) {
+            return occupied
+        } else {
+            return occupied && y != tileY;
+        }
+    } catch (e) {
+        return false;
+    }
+}
+
+
 AppController.prototype.update = function () {
     var left = Key.isDown(Key.LEFT);
     var right = Key.isDown(Key.RIGHT);
 
     if (!left && !right || left && right) {
+
+        // use x accel as friction, these if statements are to prevent
+        // oscillation
         if (this.player.vx > PLAYER_XACCEL) {
             this.player.vx -= PLAYER_XACCEL;
         } else if (this.player.vx < -PLAYER_XACCEL) {
@@ -28,14 +61,53 @@ AppController.prototype.update = function () {
     } else if (Key.isDown(Key.LEFT)) {
         if (this.player.vx > -PLAYER_CAP_XVEL) {
             this.player.vx -= PLAYER_XACCEL;
+            this.player.vx = Math.max(this.player.vx, -PLAYER_CAP_XVEL) //cap
         }
     } else if (Key.isDown(Key.RIGHT)) {
         if (this.player.vx < PLAYER_CAP_XVEL) {
             this.player.vx += PLAYER_XACCEL;
+            this.player.vx = Math.min(this.player.vx, PLAYER_CAP_XVEL) //cap
         }
     }
 
-    this.player.x += this.player.vx;
+
+    if (Key.isDown(Key.UP)) {
+        this.player.y -= .1;
+    }
+
+    if (Key.isDown(Key.DOWN)) {
+        this.player.y += .1;
+    }
+
+    //this.player.setBottom(Math.floor(6.3));
+
+    // Collision resolution
+    if (this.player.vx > 0) {
+        var potX = this.player.right() + this.player.vx; //potentialX
+
+        var wouldHitWall = this.inCell(potX, this.player.top(), false) || 
+                        this.inCell(potX, this.player.bottom(), false)
+        if (!wouldHitWall) {
+            this.player.setRight(potX);
+        } else {
+            //TODO if i'm dipped in the ground, this will cause me to scoot left!
+            this.player.setRight(Math.floor(potX));
+            this.player.vx = 0;
+        }
+    }
+    if (this.player.vx < 0) {
+        var potX = this.player.left() + this.player.vx; //potentialX
+
+        var wouldHitWall = this.inCell(potX, this.player.top(), false) || 
+                        this.inCell(potX, this.player.bottom(), false)
+        if (!wouldHitWall) {
+            this.player.setLeft(potX);
+        } else {
+            this.player.setLeft(Math.floor(potX)+1);
+            this.player.vx = 0;
+        }
+    }
+
 }
 
 AppController.prototype.draw = function () {
--- src/platform/character.js
+++ src/platform/character.js
@@ -1,28 +1,53 @@
+
 var Character = function () {
     this.x = 5;
-    this.y = 5;
-    this.width = PLAYER_WIDTH;
-    this.height = PLAYER_HEIGHT;
+    this.y = 5.1;
+    this.width = PLAYER_WIDTH/TILE;
+    this.height = PLAYER_HEIGHT/TILE;
     this.vx = 0;
     this.vy = 0;
+
+    this.touchLeft = true;
+    this.touchRight = true;
+    this.touchDown = true;
+    this.touchUp = true;
 }
 
 Character.prototype.left = function () {
     return this.x;
 }
 
+Character.prototype.setLeft = function (x) {
+    this.x = x;
+}
+
 Character.prototype.right = function () {
     return this.x + this.width;
 }
 
-Character.prototype.up = function () {
+Character.prototype.setRight = function (x) {
+    this.x = x - this.width;
+}
+
+Character.prototype.top = function () {
     return this.y;
 }
 
-Character.prototype.down = fuction () {
+Character.prototype.setTop = function (y) {
+    this.y = y;
+}
+
+Character.prototype.bottom = function () {
     return this.y + this.height;
 }
 
+Character.prototype.setBottom = function (y) {
+    this.y = y - this.height;
+}
+
 Character.prototype.draw = function () {
-    Utils.drawRect(this.x * TILE, this.y * TILE, this.width, this.height, "#238341");
+    Utils.drawRect(this.x * TILE, this.y * TILE, this.width*TILE, this.height*TILE, "#238341");
+    if (this.touchLeft) {
+        Utils.drawRect(this.x * TILE, this.y * TILE, this.width*TILE/10, this.height*TILE, "#238341");
+    }
 }
